b526af4f479df533785ce9b7c5ec301d086aa22c
Francis Beresford <francis.beresford@gmail.com>
2014-10-01 21:50:37 +0700

    menu fix

--- Module.php
+++ Module.php
@@ -4,20 +4,25 @@ namespace snapcms\mandrill\ui;
 
 class Module extends \snapcms\components\SnapCMSModule
 {   
+    public static $primaryMenu = [];
+    public static $secondaryMenu = [];
+    
     public function init()
     {
         parent::init();
         $user = \Yii::$app->user;
-        $this->primaryMenu = [
-            ['label' => 'Mailer', 'url' => ['/mailer/default/index'], 
+        static::$primaryMenu = [
+            'mailer' => ['label' => 'Mailer', 'url' => ['/mailer/default/index'], 
                 'visible' => 
                     $user->can('Use Mailer') || 
                     $user->can('Update Mailer Templates'),
                 'items' => [
-                    ['label' => 'Templates', 'url' => ['/mailer/template/index'],
-                        'visible' => $user->can('Update Mailer Templates')],
-                    ['label' => 'Subscribers', 'url' => ['/mailer/default/subscribers'],
-                        'visible' => $user->can('Use Mailer')],
+                    'templates' => ['label' => 'Templates', 'url' => ['/mailer/template/index'],
+                        'visible' => $user->can('Update Mailer Templates')
+                    ],
+                    'subscribers' => ['label' => 'Subscribers', 'url' => ['/mailer/default/subscribers'],
+                        'visible' => $user->can('Use Mailer')
+                    ],
                 ],
             ],
         ];
